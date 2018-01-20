4bb8d4cf79a281ea2316aa870fbfbcf5d53c94fd
Mukhtar Alhejji <mukhtaralhejji@TB963.WPA.Dal.Ca>
2018-01-20 18:16:18 -0400

    Added Btn1 to the UI

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Btn1 = (Button) findViewById(R.id.Btn1);
+
+        Btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText) findViewById(R.id.input);
+                TextView output = (TextView) findViewById(R.id.output);
+
+                output.setText(input.getText());
+            }
+        });
     }
 }
